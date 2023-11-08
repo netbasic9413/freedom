0fadccfd24f611be838826bdbddd5664c862b12e
netbasic9413 <netbasic@naver.com>
2023-11-09 00:12:39 +0900

    자동매매 화면 구현 및 설정 값 저장 일부 구현

--- AutoTradingConf.cpp
+++ AutoTradingConf.cpp
@@ -29,8 +29,8 @@ void CAutoTradingConf::DoDataExchange(CDataExchange* pDX)
 	DDX_Control(pDX, IDC_CHECK_BUY_MACRO, m_checkBuyMacro);
 	DDX_Control(pDX, IDC_TP_START_BUY_MACRO, m_dtpickerBuyMacroStart);
 	DDX_Control(pDX, IDC_TP_END_BUY_MACRO, m_dtpickerBuyMacroEnd);
-	DDX_Control(pDX, IDC_TP_START_TIME, m_dtpickerAutoTradingStart);
-	DDX_Control(pDX, IDC_TP_END_TIME, m_dtpickerAutoTradingEnd);
+	//DDX_Control(pDX, IDC_TP_START_TIME, m_dtpickerAutoTradingStart);
+	//DDX_Control(pDX, IDC_TP_END_TIME, m_dtpickerAutoTradingEnd);
 	DDX_Control(pDX, IDC_COMBO_BUY_MACRO, m_comboBuyMacro);
 	DDX_Control(pDX, IDC_CHECK_SEL_MACRO, m_checkSelMacro);
 	DDX_Control(pDX, IDC_CHECK_EXE_OWN, m_checkExeOwnEvent);
@@ -49,6 +49,7 @@ void CAutoTradingConf::DoDataExchange(CDataExchange* pDX)
 	DDX_Control(pDX, IDC_CHECK_EXE_PROFIT_LOSS, m_checkExeProfitLoss);
 	DDX_Control(pDX, IDC_CHECK_FAST_BUY, m_checkFastBuy);
 	DDX_Control(pDX, IDC_CHECK_EVENT_MAX_PROFIT_RATIO, m_checkEventMaxProfit);
+	DDX_Control(pDX, IDC_CHECK_AT_ONCE_SEL_TIME, m_checkAtOnceSelTime);
 }
 
 
@@ -60,8 +61,8 @@ BEGIN_MESSAGE_MAP(CAutoTradingConf, CDialogEx)
 	ON_BN_CLICKED(IDC_CHECK_BUY_MACRO, &CAutoTradingConf::OnBnClickedCheckBuyMacro)
 	ON_NOTIFY(DTN_DATETIMECHANGE, IDC_TP_START_BUY_MACRO, &CAutoTradingConf::OnDtnDatetimechangeTpStartBuyMacro)
 	ON_NOTIFY(DTN_DATETIMECHANGE, IDC_TP_END_BUY_MACRO, &CAutoTradingConf::OnDtnDatetimechangeTpEndBuyMacro)
-	ON_NOTIFY(DTN_DATETIMECHANGE, IDC_TP_START_TIME, &CAutoTradingConf::OnDtnDatetimechangeTpStartTime)
-	ON_NOTIFY(DTN_DATETIMECHANGE, IDC_TP_END_TIME, &CAutoTradingConf::OnDtnDatetimechangeTpEndTime)
+	//ON_NOTIFY(DTN_DATETIMECHANGE, IDC_TP_START_TIME, &CAutoTradingConf::OnDtnDatetimechangeTpStartTime)
+	//ON_NOTIFY(DTN_DATETIMECHANGE, IDC_TP_END_TIME, &CAutoTradingConf::OnDtnDatetimechangeTpEndTime)
 	ON_EN_CHANGE(IDC_EDIT_TOTAL_POSSIBLE_BUY_AMOUNT, &CAutoTradingConf::OnEnChangeEditTotalPossibleBuyAmount)
 	ON_CBN_SELCHANGE(IDC_COMBO_BUY_MACRO, &CAutoTradingConf::OnCbnSelchangeComboBuyMacro)
 	ON_BN_CLICKED(IDC_CHECK_SEL_MACRO, &CAutoTradingConf::OnBnClickedCheckSelMacro)
@@ -75,7 +76,6 @@ BEGIN_MESSAGE_MAP(CAutoTradingConf, CDialogEx)
 	ON_EN_CHANGE(IDC_EDIT_TRADING_STRATEGY, &CAutoTradingConf::OnEnChangeEditTradingStrategy)
 	ON_BN_CLICKED(IDC_CHECK_EVENT_PROFIT, &CAutoTradingConf::OnBnClickedCheckEventProfit)
 	ON_BN_CLICKED(IDC_CHECK_EVENT_LOSS_RATIO, &CAutoTradingConf::OnBnClickedCheckEventLossRatio)
-	ON_BN_CLICKED(IDC_CHECK_MAX_PROFIT, &CAutoTradingConf::OnBnClickedCheckMaxProfit)
 	ON_BN_CLICKED(IDC_CHECK_EVENT_MAX_LOSS_RATIO, &CAutoTradingConf::OnBnClickedCheckEventMaxLossRatio)
 	ON_BN_CLICKED(IDC_CHECK_PROFIT_MIN_EVENT_COUNT, &CAutoTradingConf::OnBnClickedCheckProfitMinEventCount)
 	ON_EN_CHANGE(IDC_EDIT_PROFIT_MIN_EVENT_COUNT, &CAutoTradingConf::OnEnChangeEditProfitMinEventCount)
@@ -83,6 +83,7 @@ BEGIN_MESSAGE_MAP(CAutoTradingConf, CDialogEx)
 	ON_BN_CLICKED(IDC_CHECK_EXE_PROFIT_LOSS, &CAutoTradingConf::OnBnClickedCheckExeProfitLoss)
 	ON_BN_CLICKED(IDC_CHECK_FAST_BUY, &CAutoTradingConf::OnBnClickedCheckFastBuy)
 	ON_BN_CLICKED(IDC_CHECK_EVENT_MAX_PROFIT_RATIO, &CAutoTradingConf::OnBnClickedCheckEventMaxProfitRatio)
+	ON_BN_CLICKED(IDC_CHECK_AT_ONCE_SEL_TIME, &CAutoTradingConf::OnBnClickedCheckAtOnceSelTime)
 END_MESSAGE_MAP()
 
 
@@ -114,19 +115,121 @@ void CAutoTradingConf::OnBnClickedBtnSaveStrategy()
 
 	CString strFileName = theApp.m_sAppPath + "/data/"+ strTradingStrategy + ".ini";
 
+	//시작시자동매매_자동실행
+	int nGetChk = m_checkAutoExe.GetCheck();
+	CString str;
+	str.Format(_T("%d"), nGetChk);
+	::WritePrivateProfileString("AUTO_TRADING_CONF", "check_exe_auto_trading", (LPCSTR)str, strFileName);
 	
+	//시작시자동매매_운영시간_시작
+	((CEdit*)GetDlgItem(IDC_EDIT_AUTOTRADING_RUNTIME_START))->GetWindowText(str);
+	if (str.GetLength() <= 0)
+	{
+		AfxMessageBox("시작시 자동매매의 운영시간을 입력 해 주세요.");
+		return;
+	}
+	::WritePrivateProfileString("AUTO_TRADING_CONF", "autotrading_runtime_start", (LPCSTR)str, strFileName);
 
-	/*
-	//::WritePrivateProfileString("AUTO_TRADING_CONF", "TRADING_STRATEGY", m_strJongCode, strFileName);
-	// 종목코드 갯수 추가
-	strIndex.Format("%d", nCnt);
-	::WritePrivateProfileString(_T("JONG_CODE"), _T("COUNT"), strIndex, strFileName);
 
-	// 종목코드 추가
-	::WritePrivateProfileString(_T("JONG_CODE"), strIndex, strCode, strFileName);
-	m_mapJongCode.SetAt(strCode, strIndex);
-	*/
-}
+	//시작시자동매매_운영시간_끝
+	((CEdit*)GetDlgItem(IDC_EDIT_AUTOTRADING_RUNTIME_END))->GetWindowText(str);
+	if (str.GetLength() <= 0)
+	{
+		AfxMessageBox("시작시 자동매매의 운영시간을 입력 해 주세요.");
+		return;
+	}
+	::WritePrivateProfileString("AUTO_TRADING_CONF", "autotrading_runtime_end", (LPCSTR)str, strFileName);
+
+
+	//자동매수조건설정_총매수가능금액
+	((CEdit*)GetDlgItem(IDC_EDIT_TOTAL_POSSIBLE_BUY_AMOUNT))->GetWindowText(str);
+	if (str.GetLength() <= 0)
+	{
+		AfxMessageBox("총 매수가능금액을 입력 해 주세요.");
+		return;
+	}
+	::WritePrivateProfileString("AUTO_BUY_COND_CONF", "total_possible_buy_amount", (LPCSTR)str, strFileName);
+
+	//자동매수조건설정_종목별매수금액
+	((CEdit*)GetDlgItem(IDC_EDIT_TOTAL_EVENT_BUY_AMOUNT))->GetWindowText(str);
+	if (str.GetLength() <= 0)
+	{
+		AfxMessageBox("종목별 매수금액 입력 해 주세요.");
+		return;
+	}
+	::WritePrivateProfileString("AUTO_BUY_COND_CONF", "total_event_buy_amount", (LPCSTR)str, strFileName);
+
+	//자동매수조건설정_최대매수종목수
+	((CEdit*)GetDlgItem(IDC_EDIT_MAX_BUY_EVENT_COUNT))->GetWindowText(str);
+	if (str.GetLength() <= 0)
+	{
+		AfxMessageBox("최대 매수종목수를 입력 해 주세요.");
+		return;
+	}
+	::WritePrivateProfileString("AUTO_BUY_COND_CONF", "max_buy_event_count", (LPCSTR)str, strFileName);
+
+	//자동매수조건설정_주당 매수최고가
+	((CEdit*)GetDlgItem(IDC_EDIT_BUY_HIGHEST_PRICE_PER_ONE))->GetWindowText(str);
+	if (str.GetLength() <= 0)
+	{
+		AfxMessageBox("주당 매수최고가를 입력 해 주세요.");
+		return;
+	}
+	::WritePrivateProfileString("AUTO_BUY_COND_CONF", "buy_highest_price_per_one", (LPCSTR)str, strFileName);
+
+	//자동매수조건설정_주당 매수최저가
+	((CEdit*)GetDlgItem(IDC_EDIT_BUY_LOWEST_PRICE_PER_ONE))->GetWindowText(str);
+	if (str.GetLength() <= 0)
+	{
+		AfxMessageBox("주당 매수최저가를 입력 해 주세요.");
+		return;
+	}
+	::WritePrivateProfileString("AUTO_BUY_COND_CONF", "buy_lowest_price_per_one", (LPCSTR)str, strFileName);
+
+	//자동매수조건설정_종목 최소거래량
+	((CEdit*)GetDlgItem(IDC_EDIT_EVENT_SMALLEST_VOLUME))->GetWindowText(str);
+	if (str.GetLength() <= 0)
+	{
+		AfxMessageBox("종목 최소거래량을 입력 해 주세요.");
+		return;
+	}
+	::WritePrivateProfileString("AUTO_BUY_COND_CONF", "event_smallest_volume", (LPCSTR)str, strFileName);
+
+
+	//자동매수조건설정_매수조건식(체크)
+	int nGetChkBuyMacro = m_checkBuyMacro.GetCheck();
+	str.Format(_T("%d"), nGetChkBuyMacro);
+	::WritePrivateProfileString("AUTO_BUY_COND_CONF", "check_buy_macro", (LPCSTR)str, strFileName);
+
+
+	//자동매수조건설정_매수조건식(시작시간)
+	((CEdit*)GetDlgItem(IDC_EDIT_BUY_MACRO_TIME_START))->GetWindowText(str);
+	if (str.GetLength() <= 0)
+	{
+		AfxMessageBox("매수조건식 시작 시간을 입력 해 주세요.");
+		return;
+	}
+	::WritePrivateProfileString("AUTO_BUY_COND_CONF", "buy_macro_time_start", (LPCSTR)str, strFileName);
+
+
+	//자동매수조건설정_매수조건식(끝시간)
+	((CEdit*)GetDlgItem(IDC_EDIT_BUY_MACRO_TIME_END))->GetWindowText(str);
+	if (str.GetLength() <= 0)
+	{
+		AfxMessageBox("매수조건식 끝 시간을 입력 해 주세요.");
+		return;
+	}
+	::WritePrivateProfileString("AUTO_BUY_COND_CONF", "buy_macro_time_end", (LPCSTR)str, strFileName);
+
+
+
+	AfxMessageBox("저장을 완료하였습니다.");
+	
+}	
+
+
+	
+	
 
 
 void CAutoTradingConf::OnBnClickedBtnOpenStrategy()
@@ -269,13 +372,6 @@ void CAutoTradingConf::OnBnClickedCheckEventLossRatio()
 }
 
 
-void CAutoTradingConf::OnBnClickedCheckMaxProfit()
-{
-	// TODO: Add your control notification handler code here
-}
-
-
-
 
 void CAutoTradingConf::OnBnClickedCheckEventMaxLossRatio()
 {
@@ -320,6 +416,12 @@ void CAutoTradingConf::OnBnClickedCheckFastBuy()
 
 
 void CAutoTradingConf::OnBnClickedCheckEventMaxProfitRatio()
+{
+	// TODO: Add your control notification handler code here
+}
+
+
+void CAutoTradingConf::OnBnClickedCheckAtOnceSelTime()
 {
 	// TODO: Add your control notification handler code here
 }
--- AutoTradingConf.h
+++ AutoTradingConf.h
@@ -33,23 +33,31 @@ public:
 	afx_msg void OnBnClickedCheckAutoExe();
 	CButton m_checkBuyMacro; //매수조건식
 	afx_msg void OnBnClickedCheckBuyMacro();
-	CDateTimeCtrl m_dtpickerBuyMacroStart;
-	afx_msg void OnDtnDatetimechangeTpStartBuyMacro(NMHDR* pNMHDR, LRESULT* pResult);
-	CDateTimeCtrl m_dtpickerBuyMacroEnd;
-	afx_msg void OnDtnDatetimechangeTpEndBuyMacro(NMHDR* pNMHDR, LRESULT* pResult);
-	CDateTimeCtrl m_dtpickerAutoTradingStart; //자동매매 운영시간 시작
+	//CDateTimeCtrl m_dtpickerAutoTradingStart; //자동매매 운영시간 시작
 	afx_msg void OnDtnDatetimechangeTpStartTime(NMHDR* pNMHDR, LRESULT* pResult);
-	CDateTimeCtrl m_dtpickerAutoTradingEnd; //자동매매 운영시간 끝
-	afx_msg void OnDtnDatetimechangeTpEndTime(NMHDR* pNMHDR, LRESULT* pResult);
-	afx_msg void OnEnChangeEditTotalPossibleBuyAmount();
+	//CDateTimeCtrl m_dtpickerAutoTradingEnd; //자동매매 운영시간 끝
+
 	CComboBox m_comboBuyMacro; //매수조건식
 	afx_msg void OnCbnSelchangeComboBuyMacro();
+	CDateTimeCtrl m_dtpickerBuyMacroStart; //매수조건식 시작시간
+	afx_msg void OnDtnDatetimechangeTpStartBuyMacro(NMHDR* pNMHDR, LRESULT* pResult);
+	CDateTimeCtrl m_dtpickerBuyMacroEnd; //매수조건식 끝시간
+	afx_msg void OnDtnDatetimechangeTpEndBuyMacro(NMHDR* pNMHDR, LRESULT* pResult);
+	
+	afx_msg void OnDtnDatetimechangeTpEndTime(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnEnChangeEditTotalPossibleBuyAmount();
+	
+
 	CButton m_checkSelMacro;	//매도조건식
 	afx_msg void OnBnClickedCheckSelMacro();
 	CButton m_checkExeOwnEvent; //보유종목 대상 실행
 	afx_msg void OnBnClickedCheckExeOwn();
-	CDateTimeCtrl m_dtpickerMinTime;
+	CDateTimeCtrl m_dtpickerMinTime; //보유종목 대상 실행 분단위 설정
 	afx_msg void OnDtnDatetimechangeTpAtOnceTime(NMHDR* pNMHDR, LRESULT* pResult);
+	CButton m_checkAtOnceSelTime; //시간일괄청산 설정
+	afx_msg void OnBnClickedCheckAtOnceSelTime();
+
+	//전체청산방식
 	CButton m_checkAllSelProfitRatio; //전체청산방식 - 익절률
 	afx_msg void OnBnClickedCheckProfitRatio();
 	CButton m_checkAllSelLossRatio; //전체청산방식 - 손절률
@@ -59,25 +67,30 @@ public:
 	afx_msg void OnBnClickedCheck8();
 	CButton m_checkAllSellLossAmount; //전체청산방식 - 손절금액(원)
 	afx_msg void OnBnClickedCheckLossAmount();
-	CEdit m_editTradingStrategy;
+	CEdit m_editTradingStrategy; //매매전략명
 	afx_msg void OnEnChangeEditTradingStrategy();
+
+	//종목청산방식
 	CButton m_checkEventProfit; //종목청산방식 - 종목익절
 	afx_msg void OnBnClickedCheckEventProfit();
-	CButton m_checkEventLoss;
+	CButton m_checkEventLoss; //종목청산방식 - 종목손절
 	afx_msg void OnBnClickedCheckEventLossRatio();
-	CButton m_checkMaxProfit;
-	afx_msg void OnBnClickedCheckMaxProfit();
-	CButton m_checkEventMaxLoss;
+	CButton m_checkEventMaxProfit; //종목청산방식 - 최대익절
+	afx_msg void OnBnClickedCheckEventMaxProfitRatio();
+	CButton m_checkEventMaxLoss; //종목청산방식 - 최대손절
 	afx_msg void OnBnClickedCheckEventMaxLossRatio();
-	CButton m_checkProfitMinEventCount;
+
+
+	CButton m_checkProfitMinEventCount; //수익률계산 초소종목수
 	afx_msg void OnBnClickedCheckProfitMinEventCount();
 	afx_msg void OnEnChangeEditProfitMinEventCount();
-	CButton m_checkCancelBuyOutStandingOrder;
+	CButton m_checkCancelBuyOutStandingOrder; //미체결 매수주문취소
 	afx_msg void OnBnClickedCheckCancelBuyOutstandingOrder();
-	CButton m_checkExeProfitLoss;
+	CButton m_checkExeProfitLoss; //매수조건식 종료후 익절, 손절 실행
 	afx_msg void OnBnClickedCheckExeProfitLoss();
-	CButton m_checkFastBuy;
+	CButton m_checkFastBuy; //번개 매수 (빠른 매수 실행)
 	afx_msg void OnBnClickedCheckFastBuy();
-	afx_msg void OnBnClickedCheckEventMaxProfitRatio();
-	CButton m_checkEventMaxProfit;
+	
+	
+	
 };
